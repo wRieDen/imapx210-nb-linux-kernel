d1a895da40e12564db42641a5791ada5d11ad0fd
Sven Wrieden <wrieden@googlemail.com>
2015-06-11 14:20:06 +0200

    force boot args, set ttSAC2 as boot console

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 644 */
+/* This file is auto generated, version 1 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#644 Mon Sep 16 09:26:22 CST 2013"
-#define LINUX_COMPILE_TIME "09:26:22"
-#define LINUX_COMPILE_BY "jiawen"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "4_WAN_QVM_Router"
+#define UTS_VERSION "#1 Thu Jun 11 14:16:55 CEST 2015"
+#define LINUX_COMPILE_TIME "14:16:55"
+#define LINUX_COMPILE_BY "wrieden"
+#define LINUX_COMPILE_HOST "Sven-PC"
+#define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.7-infoTM"
+#define UTS_RELEASE "2.6.35.7-infoTM+"
