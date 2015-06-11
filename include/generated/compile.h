536ec07230285688ef622aa94c78f316969e60f3
Sven Wrieden <wrieden@googlemail.com>
2015-06-11 16:25:43 +0200

    fix udev accept4

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 Thu Jun 11 14:37:37 CEST 2015"
-#define LINUX_COMPILE_TIME "14:37:37"
+#define UTS_VERSION "#3 Thu Jun 11 16:09:58 CEST 2015"
+#define LINUX_COMPILE_TIME "16:09:58"
 #define LINUX_COMPILE_BY "wrieden"
 #define LINUX_COMPILE_HOST "Sven-PC"
 #define LINUX_COMPILE_DOMAIN "(none)"
