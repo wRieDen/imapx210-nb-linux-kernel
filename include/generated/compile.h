150001388636cc7f47ece9e52fa035a92fa5d53d
Sven Wrieden <wrieden@googlemail.com>
2015-06-25 02:05:06 +0200

    apply ignore

--- include/generated/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 18 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 Wed Jun 24 19:15:15 CEST 2015"
-#define LINUX_COMPILE_TIME "19:15:15"
-#define LINUX_COMPILE_BY "wrieden"
-#define LINUX_COMPILE_HOST "Sven-PC"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
